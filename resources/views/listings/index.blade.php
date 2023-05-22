a0f489e76dd484ef10fb1308b00b9bef57948d96
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2023-05-22 19:43:37 +0300

    updated

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,26 +1,14 @@
 <x-layout>
-
-@include('partials._hero')
-@include('partials._search')
-
-
-{{-- @if(count($listings) == 0)
-    <p>No listings found</p>
-@endif --}}
-
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-
-@unless(count($listings) == 0 )
-
-@foreach($listings as $listing)
-    <x-listing-card :listing="$listing" />  
-@endforeach
-
-@else
-<p>No listings found</p>
-@endunless
+    @include('partials._hero')
+    @include('partials._search')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @unless (count($listings) == 0)
+            @foreach ($listings as $listing)
+                <x-listing-card :listing="$listing" />
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
     </div>
 
 </x-layout>
-
-
